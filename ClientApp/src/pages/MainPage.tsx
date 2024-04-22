5197dc0a9ed7832cef631ac950eace628f6b7688
foksurek <foksureq@gmail.com>
2024-04-22 22:29:19 +0200

    recently searched css

--- ClientApp/src/pages/MainPage.tsx
+++ ClientApp/src/pages/MainPage.tsx
@@ -1,15 +1,19 @@
-﻿import {useEffect, useState} from "react";
+﻿import {useEffect, useRef, useState} from "react";
 import {ACCOUNT__PARSE_RECENTLY_SEARCHED_RESTAURANTS} from "../ApiConst/ApiResponse.ts";
 import axios from "axios";
 import API_URLS from "../ApiConst/ApiUrls.ts";
 import {useAuth} from "../contexts/AuthContext.tsx";
 import { Link } from "react-router-dom";
-
+import BusinessCard from "../components/dashboard/BusinessCard.tsx";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 const MainPage = () => {
 
     const {user} = useAuth();
     const [recentlySearched, setRecentlySearched] = useState<ACCOUNT__PARSE_RECENTLY_SEARCHED_RESTAURANTS | string>("")
 
+    const scrollRef = useRef<HTMLDivElement>(null);
+    
     const fetchRecentlySearched = async () => {
         const restaurants = localStorage.getItem("restaurants");
         const jsonObject = JSON.parse(restaurants as unknown as string);
@@ -23,38 +27,63 @@ const MainPage = () => {
             }
         });
     }
+    
+    const handleScrollRight = () => {
+        
+        if (scrollRef.current) {
+            scrollRef.current.scrollBy({
+                left: 400,
+                behavior: 'smooth'
+            });
+
+        }
+    };
+    const handleScrollLeft = () => {
+        
+        if (scrollRef.current) {
+            scrollRef.current.scrollBy({
+                left: -400,
+                behavior: 'smooth'
+            });
+
+        }
+    };
 
     useEffect(() => {
         fetchRecentlySearched();
     }, []);
 
     if (user)
-    return(
-        <>
-            <main className="pageContainer">
-                <section id="recentlySearched">
-                    {/*TODO: Think about how good is it*/}
-                    <h2>Recently searched</h2>
-                    {typeof recentlySearched !== "string" ?
-                        (recentlySearched.data.map((restaurant) => {
-                            return (
-                                <div key={restaurant.id}>
-                                    <Link to={`/restaurant/${restaurant.id}`}>{restaurant.id}</Link>
-                                </div>
-                            )
-                        }))
-                        :
-                        (<span>{recentlySearched}</span>)
-                    }
-                </section>
-            </main>
-        </>
-    )
-    
+        return(
+            <>
+                <main className="pageContainer">
+                    <section id="recentlySearched">
+                        <h2>Recently searched</h2>
+                        <div className="recentlySearchedContainer">
+                            <ArrowBackIosIcon onClick={handleScrollLeft}/>
+                            <div className="recentlySearched" ref={scrollRef}>
+                                {typeof recentlySearched !== "string" ?
+                                    (recentlySearched.data.slice(0, 10).reverse().map((restaurant) => {
+                                        return (
+                                            <Link to={`/restaurant/${restaurant.id}`}><BusinessCard
+                                                restaurant={restaurant}/></Link>
+                                        )
+                                    }))
+                                    :
+                                    (<span>{recentlySearched}</span>)
+                                }
+                            </div>
+                            <ArrowForwardIosIcon onClick={handleScrollRight}/>
+                        </div>
+                    </section>
+                </main>
+            </>
+        )
+
     return (
         <>
             <main className="pageContainer">
-                
+
             </main>
         </>
     )
