b37e254ee2704829c19e6bc714dac70bd30cc2b4
foksurek <foksureq@gmail.com>
2024-04-23 11:48:50 +0200

    categories endpoint

---
+++ TableSpotServer/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TableSpot.Interfaces;
+using TableSpot.Models;
+
+namespace TableSpot.Controllers;
+
+[ApiController]
+[Route("Api/[controller]")]
+public class CategoryController(
+    AppDbContext appDbContext,
+    IHttpResponseJsonService httpResponseJsonService
+) : ControllerBase
+{
+    [HttpGet]
+    public async Task<object> Get()
+    {
+        var category = await appDbContext.Categories.ToListAsync();
+        return Ok(httpResponseJsonService.Ok(category.Select(c =>
+            new {
+                c.Name,
+                c.Id
+            })));
+    }
+}
